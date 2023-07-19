aaa9bf5295c1c300ffdef0a1cfcad2d1f780e58f
Manikandan312003 <106978783+Manikandan312003@users.noreply.github.com>
2023-07-19 10:09:57 +0530

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-def MinimumCostRemove(arr):
-    arr.sort(reverse=True)
-    s=0
-    for i in range(len(arr)):
-        s+=arr[i]*(i+1)
-    return s
-print(MinimumCostRemove(list(map(int,input().split()))))
